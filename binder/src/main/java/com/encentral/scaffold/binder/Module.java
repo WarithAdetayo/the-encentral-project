a1ccdd87a593d80a15abe5f3d180d76798979181
WarithAdetayo <134127897+WarithAdetayo@users.noreply.github.com>
2023-08-03 09:11:50 +0100

    Add files via upload

---
+++ binder/src/main/java/com/encentral/scaffold/binder/Module.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.encentral.scaffold.binder;
+
+import com.encentral.staffsync.configuration.permission.impl.PermissionModule;
+import com.google.inject.AbstractModule;
+import play.libs.akka.AkkaGuiceSupport;
+
+
+/**
+ * @author poseidon
+ */
+public class Module extends AbstractModule implements AkkaGuiceSupport {
+
+
+    @Override
+    protected void configure() {
+
+        super.configure();
+
+        bind(BigBang.class).asEagerSingleton();
+        install(new PermissionModule());
+
+    }
+}
